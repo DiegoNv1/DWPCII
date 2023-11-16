2fc68cfd36ed3cfd6190df0b5627e858cebd793d
DiegoNv1 <143138991+DiegoNv1@users.noreply.github.com>
2023-11-16 00:39:24 +0000

    ⭐Feat: Validación con Yup

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,3 +1,5 @@
+import log from '../../config/winston';
+
 // Action Methods
 
 // GET '/project/addForm'
@@ -13,7 +15,28 @@ const showDashboard = (req, res) => {
   res.send("🚧 UNDER CONSTRUCTION GET  '/project/showDashboard' 🚧");
 };
 
+// POST "/project/add"
+const addPost = (req, res) => {
+  // Rescatando la info del formulario
+  const { errorData: validationError } = req;
+  // En caso de haber error
+  // se le informa al cliente
+  if (validationError) {
+    log.info('Se entrega al cliente error de validación de add Project');
+    res.status(422).json(validationError);
+  } else {
+    // En caso de que pase la validación
+    // Se desestructura la información
+    // de la peticion
+    const { validData: project } = req;
+    // Se contesta la información
+    // del proyecto al cliente
+    res.status(200).json(project);
+  }
+};
+
 export default {
   addForm,
   showDashboard,
+  addPost,
 };
--- server/domains/project/project.router.js
+++ server/domains/project/project.router.js
@@ -4,6 +4,11 @@ import { Router } from 'express';
 // Importando el controlador
 import projectController from './project.controller';
 
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import projectValidator from './project.validator';
+
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -17,6 +22,16 @@ router.get(['/', '/addForm', '/add'], projectController.addForm);
 // GET '/project/showDashboard'
 // GET '/project/projects'
 router.get(['/showDashboard', '/projects'], projectController.showDashboard);
+
+// POST "/project/add"
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addPost,
+);
 
 // Exporto este tramo de ruta
 export default router;
